8595ce754a9c99c59403e1a5349e9423a5cad241
Matt Smrke <matthew.smrke@empire.ca>
2017-04-11 15:50:18 -0400

    adds ckeditor mini config

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -1,6 +1,6 @@
 CKEDITOR.editorConfig = function(config) {
   config.language = 'en';
-  config.width = '700';
+  // config.width = '700';
   config.filebrowserBrowseUrl = "/ckeditor/attachment_files";
   config.filebrowserImageBrowseLinkUrl = "/ckeditor/pictures";
   config.filebrowserImageBrowseUrl = "/ckeditor/pictures";
@@ -10,7 +10,7 @@ CKEDITOR.editorConfig = function(config) {
   config.toolbar_Pure = [
     '/', {
       name: 'basicstyles',
-      items: ['Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat']
+      items: ['Source', '-', 'Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat']
     }, {
       name: 'paragraph',
       items: ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'BidiLtr', 'BidiRtl']
@@ -28,6 +28,30 @@ CKEDITOR.editorConfig = function(config) {
       items: ['Image', 'Table', 'HorizontalRule', 'PageBreak']
     }
   ];
+
+  config.toolbar_mini = [
+    '/', {
+      name: 'basicstyles',
+      items: ['Source', '-', "Bold",  "Italic",  "Underline",  "Strike",  "-",  "Subscript",  "Superscript"]
+    },
+    {
+      name: 'paragraph',
+      items: ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock']
+    },
+    {
+      name: 'links',
+      items: ['Link', 'Unlink']
+    },
+    {
+      name: 'styles',
+      items: ['Format', 'Font', 'FontSize']
+    },
+    {
+      name: 'insert',
+      items: ['Image', 'Table', 'HorizontalRule']
+    }
+  ];
+
   config.toolbar = 'Pure';
   return true;
 };
