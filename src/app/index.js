bd677e359ea01324ec0ae2426a0af0243fe13d9b
smrkem <smrkem@gmail.com>
2017-07-12 18:25:33 -0400

    initial commit

--- src/app/index.js
+++ src/app/index.js
@@ -1,29 +1,10 @@
-import React from 'react';
+import React from "react";
 import { render } from "react-dom";
-import { Router, Route, browserHistory, IndexRoute } from "react-router";
-
-import { Root } from "./components/Root";
-import { Home } from "./components/Home";
-import { ProjectsList } from "./components/ProjectsList";
-import { Courses } from "./components/Courses";
-import { Contact } from "./components/Contact";
-
-import "../assets/main.js";
 
 class App extends React.Component {
   render() {
-    return (
-      <Router history={browserHistory}>
-        <Route path={"/"} component={Root}>
-          <IndexRoute component={Home} />
-          <Route path={"courses"} component={Courses} />
-          <Route path={"projects"} component={ProjectsList} />
-          <Route path={"contact"} component={Contact} />
-        </Route>
-      </Router>
-    );
+    return(<h1>Working</h1>);
   }
-
 }
 
 render(<App/>, window.document.getElementById("app"));
