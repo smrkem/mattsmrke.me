afb8bf049266886e2b52e47899f22293554403f1
matt-smrke <matthew.smrke@empire.ca>
2017-07-21 14:43:55 -0400

    initial course

--- src/app/components/Contact.js
+++ src/app/components/Contact.js
@@ -1,7 +1,33 @@
 import React from "react";
 
 export class Contact extends React.Component {
+  componentDidMount() {
+    var highlights = document.getElementsByClassName("highlight");
+    var doc = document.getElementById("contact");
+    Array.from(highlights).forEach((elem) => {
+      elem.addEventListener('mouseover', function() {
+        this.classList.add("hovered");
+        doc.classList.add("faded");
+      });
+      elem.addEventListener('mouseout', function() {
+        this.classList.remove("hovered");
+        doc.classList.remove("faded");
+      });
+    });
+  }
+
   render() {
-    return(<p>Contact content</p>);
+    return(
+      <div id="main-wr">
+        <div id="contact" className="main main-content">
+          <p>Drop me a <span className="highlight highlight-pink">Message</span>.</p>
+          <p>
+            Email me at <a target="_blank" href="mailto:smrkem@gmail.com" className="highlight highlight-blue">smrkem@gmail.com</a>.
+            Check out my work on <a target="_blank" className="highlight highlight-green" href="https://github.com/smrkem">Github</a>.
+            Connect with me on <a target="_blank" className="highlight highlight-red" href="https://ca.linkedin.com/in/matthew-smrke-55692957">LinkedIn</a>.
+          </p>
+        </div>
+      </div>
+    );
   }
 }
--- src/app/components/Courses.js
+++ src/app/components/Courses.js
@@ -1,7 +1,58 @@
 import React from "react";
+import { Course } from "./Course";
 
 export class Courses extends React.Component {
   render() {
     return(<p>Courses content</p>);
+  }
+
+  getCourses() {
+    return [
+      {
+        title: "Angular 2 + Rails 5 Bootcamp",
+        status: "In progress",
+        provider: "Udemy",
+        coursePage: "https://www.udemy.com/angular-on-rails-course",
+        review: "A fast-paced walkthrough for an Angular app that communicates with 2 different Rails API services. The rails parts goes into no detail and just scaffolds everything. The Angular parts have more information, but explanations are often outdated, too shallow or in some cases incorrect.",
+      },
+      {
+        title: "AWS Certified Solutions Architect",
+        status: "Completed",
+        provider: "A Cloud Guru",
+        coursePage: "https://acloud.guru/course/aws-certified-solutions-architect-associate",
+        completionDate: "May 2017",
+        certificateUrl: "todo",
+        review: "A great introduction to AWS services and information about the AWS exams and certification process. The course gives a good overview of the relevant AWS services, and is followed up with hands-on labs and practice exams. While this course, on its own, is probably not enough to pass the exam - the discussion boards are great and point to more than enough supplementary learning resources to master the material."
+      },
+      {
+        title: "Using Python for Research",
+        status: "Completed",
+        provider: "edX",
+        coursePage: "https://www.edx.org/course/using-python-research-harvardx-ph526x",
+        syllabus: "https://courses.edx.org/courses/course-v1:HarvardX+PH526x+3T2016/4bdcc373b7a944f8861a3f190c10edca/",
+        completionDate: "December 2016",
+        certificateUrl: "todo",
+        review: "This course was fantastic. After a brief refresher on some python tools (NumPy, SciPy, Matplotlib), it dives right into using them to analyze some real and very interesting case data. DNA Translation, Language Processing, Classification and Social Network Analysis - each case study introduces new topics like principal component analysis or k-nearest neighbor classification. The instructor is clear, knowledgeable and engaging and the case study material is easily digestable but compelling enough that I found myself doing a little extra reading outside the scope of the course."
+      },
+      {
+        title: "Scaling Docker on AWS",
+        status: "Completed",
+        provider: "Udemy",
+        coursePage: "https://www.udemy.com/scaling-docker-on-aws/",
+        completionDate: "March 2017",
+        certificateUrl: "todo",
+        review: "This course was a very hands-on guide to deploying a dockerized app on a load-balanced cluster in AWS. I love that it used the command-line for all of the AWS stuff - that goes a long way towards keeping things relevant as AWS updates. While explanations didn't go very deep - all the steps were there and it wasn't difficult to find a lot of supplementary information online."
+      },
+      {
+        title: "Introduction to Python for Data Science",
+        status: "Completed",
+        provider: "edX",
+        coursePage: "https://www.edx.org/course/introduction-python-data-science-microsoft-dat208x-5",
+        syllabus: "https://courses.edx.org/courses/course-v1:Microsoft+DAT208x+5T2016/a83dfa3e93a34598987774b010e84043/",
+        completionDate: "",
+        certificateUrl: "todo",
+        review: ""
+      }
+    ];
   }
 }
