8c0fdd3a33368f716f98d77d5e5b980c29d3b672
matt <smrkem@gmail.com>
2017-07-29 13:57:58 -0400

    update home

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -23,19 +23,16 @@ export class Home extends React.Component {
         <div id="home" className="main">
           <div className="main-content">
             <p>
-              I'm a web developer in <span className="highlight highlight-green">Toronto</span>, Canada.
+              I'm a web developer in <span className="highlight highlight-green">Toronto</span>, Canada. Currently I'm focused on working with <span className="highlight highlight-red">Ruby on Rails</span> and on learning <span className="highlight highlight-orange">React</span>.  I also work with <span className="highlight highlight-pink">Flask</span> and <span className="highlight highlight-blue">Django</span>, and <span className="highlight highlight-green">python</span> in general.
             </p>
             <p>
-              Currently I'm focused on working with <span className="highlight highlight-red">Ruby on Rails</span> and on learning <span className="highlight highlight-orange">React</span>.  I also work with <span className="highlight highlight-pink">Flask</span> and <span className="highlight highlight-blue">Django</span>, and <span className="highlight highlight-green">python</span> in general for data science and <span className="highlight highlight-red">AWS lambda</span> functions.
+              I'm well versed in modern app architecture and feel that front-end frameworks like <span className="highlight highlight-red">React</span> and <span className="highlight highlight-orange">Angular</span> are must-haves<span className="long-dash"></span>especially as <span className="highlight highlight-pink">serverless</span> becomes more popular. I'm a big fan of <span className="highlight highlight-blue">Docker</span>, which has freed me to play around with a variety of languages and frameworks. I write tests for my code and develop using <span className="highlight highlight-green">TDD</span>. Here are some samples of my <Link to={"/projects"} className="highlight highlight-red">projects</Link>.
             </p>
             <p>
-              I'm well versed in modern app architecture and feel that front-end frameworks like <span className="highlight highlight-orange">React</span> and <span className="highlight highlight-pink">Angular</span> are must-haves<span className="long-dash"></span>especially as <span className="highlight highlight-blue">serverless</span> becomes more popular. I'm a big fan of <span className="highlight highlight-green">Docker</span>, which has freed me to play around with a variety of languages and frameworks. I write tests for my code and develop using <span className="highlight highlight-red">TDD</span>. Here are some samples of my <Link to={"/projects"} className="highlight highlight-orange">projects</Link>.
+              I'm an <span className="highlight highlight-orange">AWS Certified Solutions Architect - Associate</span>. I've recently begun to explore <span className="highlight highlight-pink">AWS services</span> and it's opened up whole new areas for me. It's a natural fit for <span className="highlight highlight-blue">serverless</span> and some of the other things I'm interested in<span className="long-dash"></span>like <span className="highlight highlight-green">data science</span> and <span className="highlight highlight-red">machine learning</span>.
             </p>
             <p>
-              I'm an <span className="highlight highlight-pink">AWS Certified Solutions Architect - Associate</span>. I've recently begun to explore <span className="highlight highlight-blue">AWS services</span> and it's opened up whole new areas for me. It's a natural fit for <span className="highlight highlight-green">serverless</span> and some of the other things I'm interested in<span className="long-dash"></span>like <span className="highlight highlight-red">data science</span> and <span className="highlight highlight-orange">machine learning</span>.
-            </p>
-            <p>
-              I love to learn. I'm a big advocate of online learning through <span className="highlight highlight-pink">open courseware</span><span className="long-dash"></span>it's been a huge win. Here's a list of <Link to={"/courses"} className="highlight highlight-blue">courses</Link> I've completed and am currently taking.
+              I love to learn. I'm a big advocate of online learning through <span className="highlight highlight-orange">open courseware</span><span className="long-dash"></span>it's been a huge win. Here's a list of <Link to={"/courses"} className="highlight highlight-pink">courses</Link> I've completed and am currently taking.
             </p>
           </div>
         </div>
